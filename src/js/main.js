d78174476630f7155d4e4e53e1a72c79d7a94b29
Roman Amelin <romanamelin@Romans-MacBook-Pro.local>
2021-10-24 17:15:58 +0200

    Final

--- src/js/main.js
+++ src/js/main.js
@@ -17,6 +17,20 @@ $(document).ready(function(){
     });
 
 
+    // Validace formuláře
+
+    $('#brief-form').validate({
+      rules: {
+        username: "required",
+        phone: "required",
+      },
+      messages: {
+        username: "Uveďte vaše jméno",
+        phone: "Uveďte váš telefon"
+      }
+    });
+
+
     // Napojení slideru
     $('.slider').slick({
         slidesToShow: 3,
